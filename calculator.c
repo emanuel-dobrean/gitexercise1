5642b42899105007877d43ca9857aa9424ef523f
emanuel-dobrean <73114671+emanuel-dobrean@users.noreply.github.com>
2020-11-05 11:33:41 +0100

    Ausbessern

--- calculator.c
+++ calculator.c
@@ -3,10 +3,12 @@
 /**
 * Simple calculator
 */
-int main(){
+int main()
+
+{
 
 	float num1;
-	int num2;
+	float num2;
 	float ans;
 	char op;
 	printf("\nEnter an operator (+,-,/,*): \n");
@@ -15,17 +17,32 @@ int main(){
 	printf("\nEnter two numbers: ");
 	scanf("%f",&num1);
 	scanf("%f",&num2);
-	
+
 	switch(op){
-		case '+' : 
+		case '+' :
 			ans = num1 + num2;
 			break;
-		default:
+
+		case '-' :
+			ans = num1 - num2;
+			break;
+
+		case '*' :
+			ans = num1 * num2;
+			break;
+
+        case '/' :
+			ans = num1 / num2;
+			break;
+
+        default:
 			printf("Enter a correct operator!");
 			return 1;
-	
-	}
+			break;
+
+}
 	printf("\n The result is as follows:\n");
 	printf("%f %c %f = %f",num1, op ,num2,ans);
 	return 0;
+
 }
